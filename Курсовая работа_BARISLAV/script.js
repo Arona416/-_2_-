f3f9d3e2e0e36c5e2d1c9120647fa6a0628c86e1
arona <aronadakar7@gmail.com>
2023-11-04 23:44:01 +0400

    заказ кофе

---
+++ Курсовая работа_BARISLAV/script.js
@@ -0,0 +1,39 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchform.classList.remove('active');
+    cartItem.classList.remove('active');
+
+}
+
+let searchform = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchform.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+    
+}
+
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchform.classList.remove('avtive');
+
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    searchform.classList.remove('active');
+    cartItem.classList.remove('active');
+
+}
+function toggleDarkMode(){
+    var footer =document.querySelector('footer');
+    footer.classList.toggle('dark-mode');
+ }
+
+
